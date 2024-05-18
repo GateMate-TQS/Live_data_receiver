eaa7d037ff51258efd9bbaa1f0ba3fbb85bd8f59
andreaoliveira9 <andreaoliveira@ua.pt>
2024-05-18 22:17:22 +0100

    fetch active flights

--- api/src/main/java/gatemate/LiveData.java
+++ api/src/main/java/gatemate/LiveData.java
@@ -8,6 +8,11 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.HttpClientErrorException;
@@ -24,7 +29,7 @@ public class LiveData {
     private String apiKey;
 
     private static final String DEP_ICAO = "LPPT";
-    private static final String FLIGHT_STATUS = "scheduled";
+    private static final List<String> FLIGHT_STATUS = List.of("active", "scheduled");
     private static final String API_URL = "http://api.aviationstack.com/v1/flights?access_key=";
 
     private static final String QUEUE_NAME = "flight-data";
@@ -38,17 +43,18 @@ public class LiveData {
     public void fetchDataAndSendToQueue() {
         System.out.println("Fetching data from API");
         RestTemplate restTemplate = new RestTemplate();
-        String url = API_URL + apiKey + "&dep_icao=" + DEP_ICAO + "&flight_status=" + FLIGHT_STATUS;
-
-        try {
-            ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-            String jsonData = response.getBody();
-            rabbitTemplate.convertAndSend(QUEUE_NAME, jsonData);
-            System.out.println("Data sent to queue");
-        } catch (HttpClientErrorException | HttpServerErrorException e) {
-            System.err.println("Error accessing API: " + e.getRawStatusCode() + " - " + e.getStatusText());
-        } catch (Exception e) {
-            System.err.println("Unexpected error: " + e.getMessage());
+        for (int i = 0; i < FLIGHT_STATUS.size(); i++) {
+            String url = API_URL + apiKey + "&dep_icao=" + DEP_ICAO + "&flight_status=" + FLIGHT_STATUS.get(i);
+            try {
+                ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+                String jsonData = response.getBody();
+                rabbitTemplate.convertAndSend(QUEUE_NAME, jsonData);
+                System.out.println("Data sent to queue");
+            } catch (HttpClientErrorException | HttpServerErrorException e) {
+                System.err.println("Error accessing API: " + e.getRawStatusCode() + " - " + e.getStatusText());
+            } catch (Exception e) {
+                System.err.println("Unexpected error: " + e.getMessage());
+            }
         }
     }
 }
